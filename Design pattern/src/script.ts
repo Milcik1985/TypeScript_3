864b28a8032edbc6203b631ec5e81d81d4d26a34
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-03-12 10:14:59 +0200

    deign_pattern

---
+++ Design pattern/src/script.ts
@@ -0,0 +1,5 @@
+// 1. Implement a Logger class using the Singleton pattern. Logger should have acessible id. // check if after 2 logs id still the same;
+// 2. Create a GameCharacter factory class. Based on a given text input to the  GameCharacter class it should return a following classes "HUMAN", "ANIMAL", 'WIZZARD';
+// 3. Create a SmartHomeFacade class that simplifies the interface for controlling various smart home devices (lights, thermostat, security system) with unified methods like morningRoutine() leaveHome(), and nightMode().
+
+// 4. Create a Tesla builder class. Car should have "wheels", "seatType", "color", "soundSystem". All of those properties should have default values. With TeslaBuilderClass you should be able to modify it's properties.
